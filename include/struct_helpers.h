12a69f8edb21964f94f79968e3fc722128a92a5c
Safin Stefanos <safin.n.stefanos.28@dartmouth.edu>
2026-05-14 18:56:08 -0400

    Update struct_helpers.h

--- include/struct_helpers.h
+++ include/struct_helpers.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Queue
 typedef struct q_node {
     void *data;
     size_t data_size;
@@ -8,12 +9,12 @@ typedef struct q_node {
 } q_node;
 
 typedef struct Queue {
-    QueueNode *front;
-    QueueNode *rear;
+    q_node *front;
+    q_node *rear;
     int size;
 } Queue;
 
-Queue *initQueue() {
+Queue *init_queue() {
     Queue *q = (Queue *)malloc(sizeof(Queue));
     if (q == NULL) {
         return NULL;
@@ -22,8 +23,25 @@ Queue *initQueue() {
     q->front = NULL;
     q->rear = NULL;
     q->size = 0;
-
     return q;
+}
+
+queue_node *create_queue_node(void *data, size_t data_size) {
+    queue_node *node = malloc(sizeof(queue_node));
+    if (node == NULL) {
+        return NULL;
+    }
+
+    node->data = malloc(data_size);
+    if (node->data == NULL) {
+        free(node);
+        return NULL;
+    }
+
+    memcpy(node->data, data, data_size);
+    node->data_size = data_size;
+    node->next = NULL;
+    return node;
 }
 
 int isEmpty(Queue *q) {
@@ -31,44 +49,41 @@ int isEmpty(Queue *q) {
 }
 
 int enqueue(Queue *q, void *data, size_t data_size) {
-    QueueNode *newNode = (QueueNode *)malloc(sizeof(QueueNode));
-
-    if (newNode == NULL) {
-        return 0;
+    q_node *new_node = (q_node *)malloc(sizeof(q_node));
+    if (new_node == NULL) {
+        return 1;
     }
 
-    newNode->data = malloc(data_size);
-
-    if (newNode->data == NULL) {
-        free(newNode);
-        return 0;
+    new_node->data = malloc(data_size);
+    if (new_node->data == NULL) {
+        free(new_node);
+        return 1;
     }
-
-    memcpy(newNode->data, data, data_size);
-    newNode->data_size = data_size;
-    newNode->next = NULL;
+    
+    memcpy(new_node->data, data, data_size);
+    new_node->data_size = data_size;
+    new_node->next = NULL;
 
     if (isEmpty(q)) {
-        q->front = newNode;
-        q->rear = newNode;
-    } else {
-        q->rear->next = newNode;
-        q->rear = newNode;
+        q->front = new_node;
+        q->rear = new_node;
     }
-
+    else {
+        q->rear->next = new_node;
+        q->rear = new_node;
+    }
     q->size++;
-
-    return 1;
+    return 0;
 }
 
 int dequeue(Queue *q, void *out_buffer, size_t buffer_size) {
     if (isEmpty(q)) {
-        return 0;
+        return 1;
     }
 
-    QueueNode *temp = q->front;
+    q_node *temp = q->front;
     if (buffer_size < temp->data_size) {
-        return 0;
+        return 1;
     }
 
     memcpy(out_buffer, temp->data, temp->data_size);
@@ -80,7 +95,7 @@ int dequeue(Queue *q, void *out_buffer, size_t buffer_size) {
     free(temp->data);
     free(temp);
     q->size--;
-    return 1;
+    return 0;
 }
 
 void *peek(Queue *q) {
@@ -91,16 +106,132 @@ void *peek(Queue *q) {
     return q->front->data;
 }
 
-int queueSize(Queue *q) {
+int queue_size(Queue *q) {
     return q->size;
 }
 
-void destroyQueue(Queue *q) {
+void destroy_queue(Queue *q) {
     while (!isEmpty(q)) {
-        QueueNode *temp = q->front;
+        q_node *temp = q->front;
         q->front = temp->next;
         free(temp->data);
         free(temp);
     }
     free(q);
+}
+
+// Linked List
+typedef struct list_node {
+    void *data;
+    size_t data_size;
+    struct list_node *next;
+} list_node;
+
+typedef struct LinkedList {
+    list_node *head;
+    int size;
+} LinkedList;
+
+/* Create list */
+LinkedList *init_list() {
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    if (list == NULL) {
+        return NULL;
+    }
+    list->head = NULL;
+    list->size = 0;
+
+    return list;
+}
+
+list_node *create_list_node(void *data, size_t data_size) {
+    list_node *node = (list_node *)malloc(sizeof(list_node));
+    if (node == NULL) {
+        return NULL;
+    }
+
+    node->data = malloc(data_size);
+    if (node->data == NULL) {
+        free(node);
+        return NULL;
+    }
+
+    memcpy(node->data, data, data_size);
+    node->data_size = data_size;
+    node->next = NULL;
+    return node;
+}
+
+int push_front(LinkedList *list, void *data, size_t data_size) {
+    list_node *node = createNode(data, data_size);
+    if (node == NULL) {
+        return 1;
+    }
+
+    node->next = list->head;
+    list->head = node;
+    list->size++;
+    return 0;
+}
+
+int push_back(LinkedList *list, void *data, size_t data_size) {
+    list_node *node = createNode(data, data_size);
+    if (node == NULL) {
+        return 1;
+    }
+
+    if (list->head == NULL) {
+        list->head = node;
+    } 
+    else {
+        list_node *curr = list->head;
+        while (curr->next != NULL) {
+            curr = curr->next;
+        }
+        curr->next = node;
+    }
+
+    list->size++;
+    return 0;
+}
+
+int popFront(LinkedList *list, void *out_buffer, size_t buffer_size) {
+    if (list->head == NULL) {
+        return 1;
+    }
+
+    list_node *temp = list->head;
+    if (buffer_size < temp->data_size) {
+        return 1;
+    }
+
+    memcpy(out_buffer, temp->data, temp->data_size);
+    list->head = temp->next;
+    free(temp->data);
+    free(temp);
+    list->size--;
+    return 0;
+}
+
+list_node *getNode(LinkedList *list, int index) {
+    if (index < 0 || index >= list->size) {
+        return NULL;
+    }
+    list_node *curr = list->head;
+    for (int i = 0; i < index; i++) {
+        curr = curr->next;
+    }
+
+    return curr;
+}
+
+void destroy_llist(LinkedList *list) {
+    list_node *curr = list->head;
+    while (curr != NULL) {
+        list_node *temp = curr;
+        curr = curr->next;
+        free(temp->data);
+        free(temp);
+    }
+    free(list);
 }
