c998f63f9ba5137370fb78414d97a3dbb2afba55
Safin Stefanos <safin.n.stefanos.28@dartmouth.edu>
2026-05-14 20:09:08 -0400

    Add comments for kernel function documentation
    Added detailed comments for kernel functions including SetKernelBrk, KernelStart, LoadProgram, and KCSwitch.

--- src/kernel.c
+++ src/kernel.c
@@ -2,3 +2,54 @@
 #include <traps.h>
 #include <load_info.h>
 #include <yalnix.h>
+
+/*
+
+SetKernelBrk
+	Check if virtual memory is enabled
+	Compute page indices
+  If more pages needed, check if enough physical frames are free then allocate, else undo partially allocated pages and error
+
+	
+KernelStart
+	Init free frame tracker, process queues
+	Build region 0 page table
+	Create void* array for Interrupt Vector table where each entry points to a handler
+	Write table address to REG_VECTOR_BASE register
+  Init first proc, create PCB, alloc kernel stack, alloc region 1 page table with one page for user stack
+  Compute kernel mem boundaries
+  Map kernel heap, data, text pages
+  Init region 0 page table
+  Write regions into REG_PTRs
+  set REG_VM_ENABLE to 1
+  Create init process PCB
+  Load init program
+  Flush tlb
+
+LoadProgram
+	Open exec file
+	Read exec file metadata
+	Compute arg sizes
+	Save argos
+	Compute stack layout
+	Calc required virtual pages
+	Check if mem requirements are doable
+	Init user stack pointer
+	Destroy old addr space
+	Allocate text, data, bss, stack pages
+	Flush TLB
+	Load program contents/PCB
+	Swap text pages to read only
+	Set program counter
+	Build argc/argv into user stack
+	Clear out CPU state for program
+
+KCSwitch
+	Validate PCB pointers
+	Save current proc kernel context
+	Validate next proc kernel context
+	Switch kernel stack mappings
+	Flush tlb
+	Reset scheduling tick count
+
+*/
