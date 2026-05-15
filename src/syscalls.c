88fc28f0b2bed20a043dfbe97b247020ee6f2a4d
Safin Stefanos <safin.n.stefanos.28@dartmouth.edu>
2026-05-14 20:03:03 -0400

    Reformat comments and include headers in syscalls.c

--- src/syscalls.c
+++ src/syscalls.c
@@ -14,14 +14,14 @@ TRAP_KERNEL
 	Put return value in regs in UserContext
 TRAP_CLOCK
 	Global tick counter ++
-  Check for procs waiting on delay and wake them if enough time has passed
-  Implement round robin scheduling
+  	Check for procs waiting on delay and wake them if enough time has passed
+  	Implement round robin scheduling
 TRAP_ILLEGAL
 	Print illegal instruction error and exit
 TRAP_MEMORY
-	Check if faulty addr is in region 1 and sits between current brk and bottom of user stack
-  If valid growth, allocate new frame and update region 1 page table, then return and retry instruction
-  Else abort
+  	Check if faulty addr is in region 1 and sits between current brk and bottom of user stack
+  	If valid growth, allocate new frame and update region 1 page table, then return and retry instruction
+  	Else abort
 TRAP_MATH
 	Print illegal math operation and exit
 TRAP_TTY_RECEIVE
@@ -32,8 +32,8 @@ TRAP_TTY_RECEIVE
 	Context switch to waiting proc if there is one
 	Restore original proc
 TRAP_TTY_TRANSMIT
-  Find interrupt terminal ID
-  Find completed write request
-  Wake blocked writer proc
+  	Find interrupt terminal ID
+  	Find completed write request
+  	Wake blocked writer proc
 
 */
