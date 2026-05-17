b4801848b2db4d846882afcb574f69b391ea82e7
eisch28-cpu <eigen.h.schinaman.28@dartmouth.edu>
2026-05-17 19:00:56 -0400

    Update procs.h

--- include/procs.h
+++ include/procs.h
@@ -2,3 +2,19 @@
 #include <pagetable.h>
 #include <stdbool.h>
 
+#ifndef _PCB_H
+#define _PCB_H
+
+#define S_RUNNING 0
+#define S_READY   1
+#define S_BLOCKED 2
+
+typedef struct PCB{
+  UserContext usr_ctx;
+  KernelContext krn_ctx;
+  pte_t *r1pt;
+  int pid;
+  int state;
+}PCB_t;
+
+#endif
