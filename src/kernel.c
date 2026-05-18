d18f097980feaa0534c4bcd653d2ccea93a58ad7
eisch28-cpu <eigen.h.schinaman.28@dartmouth.edu>
2026-05-18 11:17:25 -0400

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -67,73 +67,72 @@ KCSwitch
 extern void KernelStart (char **argv, unsigned int pmem_size, UserContext *ctx){
 	TracePrintf(DEBUG, "KernelStart\n");
 
-	int num_frames = pmem_size / PAGESIZE; /*how many r actually possible?*/
+	int num_frames = pmem_size/PAGESIZE; /*how many r actually possible?*/
 	int i;
-	for(i = 0; i < (MAX_PMEM_SIZE / PAGESIZE); i++){ /*make free*/
+	for(i=0; i<num_frames; i++){ /*make free*/
     	frames[i] = 0;
 	}
-	for(i = 0; i < _orig_kernel_brk_page; i++){
+	
+	for(i = 0; i < _orig_kernel_brk_page; i++){ /*kernel space*/
     	frames[i] = 1;
 	}
-
-	frames[KERNEL_STACK_BASE >> PAGESHIFT] = 1; /*bc kernel stack is 2 pages*/
-	frames[(KERNEL_STACK_BASE >> PAGESHIFT) + 1] = 1;
 	
 	for (i = 0; i < MAX_PT_LEN; i++){
     	KernelPT[i].valid = 0; /*Not used yet*/
 	}
 
-	for(i=_first_kernel_text_page; i< _first_kernel_data_page; i++){
+	for(i=_first_kernel_text_page; i< _first_kernel_data_page; i++){ /*text*/
     	KernelPT[i].valid = 1;
    		KernelPT[i].pfn = i; /*VPN i -> PFN i*/
     	KernelPT[i].prot = PROT_READ | PROT_EXEC;
 	}
-	for(i=_first_kernel_data_page; i<_orig_kernel_brk_page; i++){
+	for (i = _orig_kernel_brk_page; i < (UP_TO_PAGE(sbrk(0)) >> PAGESHIFT); i++) { /*data*/
+    	KernelPT[i].valid = 1;
+    	KernelPT[i].pfn = i; 
+    	KernelPT[i].prot = PROT_READ | PROT_WRITE;
+	}
+	
+	for(i=_first_kernel_data_page; i<_orig_kernel_brk_page; i++){ /*heap*/
     	KernelPT[i].valid = 1;
     	KernelPT[i].pfn = i; /*map*/
     	KernelPT[i].prot = PROT_READ | PROT_WRITE;
+		frames[i] = 1;
 	}
 	
 	for(i = 0; i < TRAP_VECTOR_SIZE; i++){
     	IVT[i] = &thandler; 
 	}
-
 	WriteRegister(REG_VECTOR_BASE, (unsigned int)IVT);
 	
-	// Clear the Region 1 table
-for(i=0; i<MAX_PT_LEN; i++){
-    r1pt[i].valid = 0;
-	}
-
 	
-	int idle_stack_frame = _orig_kernel_brk_page + 2; /*user stack gets first free page/frame*/
-	frames[idle_stack_frame] = 1; /*now in use*/
-
-	/*map last page of region 1 to this frame*/
-	r1pt[MAX_PT_LEN-1].valid = 1;
-	r1pt[MAX_PT_LEN-1].pfn = idle_stack_frame;
-	r1pt[MAX_PT_LEN-1].prot = PROT_READ | PROT_WRITE;
-
-	/*map to hardware*/
-	WriteRegister(REG_PTBR1, (unsigned int)r1pt);
+	current_process = (PCB_t *)malloc(sizeof(PCB_t)); /*Init process PCB*/
+	current_process->r1pt = (pte_t *)malloc(sizeof(pte_t) * MAX_PT_LEN);
+	
+	for(i = 0; i < MAX_PT_LEN; i++){/*clear r1 table*/
+    	current_process->r1pt[i].valid = 0;
+	}
+	
+	WriteRegister(REG_PTBR1, (unsigned int)current_process->r1pt);
 	WriteRegister(REG_PTLR1, MAX_PT_LEN);
 	WriteRegister(REG_PTBR0, (unsigned int)KernelPT);
 	WriteRegister(REG_PTLR0, MAX_PT_LEN);
 
-	int kstack_idx= KERNEL_STACK_BASE >> PAGESHIFT;
-	KernelPT[kstack_idx].valid = 1;
-	KernelPT[kstack_idx].pfn = kstack_idx;
-	KernelPT[kstack_idx].prot = PROT_READ | PROT_WRITE;
-	KernelPT[kstack_idx+1].valid = 1;
-	KernelPT[kstack_idx+1].pfn = kstack_idx+1;
-	KernelPT[kstack_idx+1].prot = PROT_READ | PROT_WRITE;
+	int kstack_idx = KERNEL_STACK_BASE >> PAGESHIFT;
+	for (i = 0; i < 2; i++) { /*allocate for 2 frames*/
+  		int f = find_free(); /*have not made yet*/
+    	frames[f] = 1;
+    	KernelPT[kstack_idx + i].valid = 1;
+    	KernelPT[kstack_idx + i].pfn = f; 
+    	KernelPT[kstack_idx + i].prot = PROT_READ | PROT_WRITE;
+	}
 
-	ipcb=(pcb_t *)malloc(sizeof(pcb_t));
-    ipcb->pid = helper_new_pid(r1pt); /*pid*/
-    WriteRegister(REG_VM_ENABLE, 1); [4] /*ENABLE THE BIG VM*/
+	WriteRegister(REG_VM_ENABLE, 1);/*ENABLE THE BIG VM*/
+	WriteRegister(REG_TLB_FLUSH, TLB_FLUSH_ALL); /*tmv flush*/
 
-    ctx->pc = (void *)&idlin;      /*idle*/
-    ctx->sp = (void *)VMEM_1_LIMIT; /*stack is at top of region 1 [2]*/
+    if (LoadProgram(argv, argv, ctx) != LI_NO_ERROR) {
+    	TracePrintf(0, "Failed to load init program\n");
+    	Halt();
+	} /*stack is at top of region 1 [2]*/
 
     TracePrintf(0, "Leaving KernelStart, entering user mode...\n");
 	
