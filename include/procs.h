d18236f460116319df18d29a9add3e0092dcadae
eisch28-cpu <eigen.h.schinaman.28@dartmouth.edu>
2026-05-18 10:51:57 -0400

    Update procs.h

--- include/procs.h
+++ include/procs.h
@@ -14,8 +14,10 @@ typedef struct PCB{
   KernelContext krn_ctx;
   pte_t *r1pt;
   int pid;
+  int ppid;
   int state;
-  void *kernel_stack_base;
+  int exstat;
+  int kstack_pfn[4];
   struct PCB *child_pcb->parent = current_process;
   struct PCB *child_pcb->next_sibling = current_process->children;
   struct PCB *current_process->children = child_pcb;
