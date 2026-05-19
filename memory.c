23b65992d30ccd13f3c6663098018467949d2de8
Safin Stefanos <safin.n.stefanos.28@dartmouth.edu>
2026-05-19 17:19:54 -0400

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,1 +1,18 @@
 
+int find_free() {
+	for (int j = 0; j < MAX_PMEM_SIZE/PAGESIZE; j++) {
+		if (frames[j] == 0) return j;
+	}
+	return ERROR;
+}
+
+
+
+static void free_region1(pte_t *pt) {
+    for (int i = 0; i < MAX_PT_LEN; i++) {
+        if (pt[i].valid && i >= (VMEM_1_BASE >> PAGESHIFT)) {
+            frames[pt[i].pfn] = 0;
+            pt[i].valid = 0;
+        }
+    }
+}
